fc2365ed42338fbb9b28463107a3a31914d893c4
cmonyeba <40518793+cmonyeba@users.noreply.github.com>
2021-08-22 13:45:41 -0700

    added privacy policies

--- next-app/components/Footer.js
+++ next-app/components/Footer.js
@@ -1,20 +1,19 @@
 import React, { useState } from 'react'
+import Link from 'next/link'
 
 const Footer = () => {
 
     // const [show, setShow] = useState(false)
     return (
         <> 
-        <footer className="flex flex-row flex-wrap relative items-center justify-center w-full h-24 border-t">
+        <footer className="flex flex-col flex-wrap relative items-center justify-center w-full h-24 border-t">
             <span className='font-semibold'>
             Powered by{' '}
             </span>
-            <p className='mx-1 text-blue-600 font-semibold'>Scotty's Love &#128150;</p>
-            {/* <p className='ml-6 absolute left-0 mr-64 ' onClick={()=>setShow(!show)}>
-                {
-                show ? <a className='transform translate-y-4'>@cmonyeba</a> : <p>♨️</p>                
-                }
-            </p> */}
+            <p className='mx-1 text-blue-600 font-semibold'>Scotty's Soul &#128150;</p>
+            <Link href='/privacy'>
+            <a>Privacy</a>
+            </Link>
         </footer>
       </>
     )
